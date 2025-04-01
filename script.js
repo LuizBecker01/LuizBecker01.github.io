ccd1a62cfa9f7bfe2e765cc6cdcf28171f176d87
Luiz Becker <luizhenriquebecker000@gmail.com>
2025-04-01 19:07:01 -0300

    ...

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const texto = "Olá! Meu nome é Luiz Becker, seja bem-vindo ao meu portifólio de trabalhos!";
-        let i = 0;
-        function digitar() {
-            if (i < texto.length) {
-                document.getElementById("chat").textContent += texto.charAt(i);
-                i++;
-                setTimeout(digitar, 100); // Ajuste a velocidade da digitação
-            } else {
-                document.getElementById("chat").style.borderRight = "none"; // Remove o cursor após a digitação
-            }
+document.addEventListener("DOMContentLoaded", function () {
+    const texto = "Olá! Meu nome é Luiz Becker.";
+    let index = 0;
+    const elemento = document.getElementById("digitando");
+
+    function digitar() {
+        if (index < texto.length) {
+            elemento.innerHTML += texto.charAt(index);
+            index++;
+            setTimeout(digitar, 220); // Velocidade da digitação
         }
-        // setTimeout(digitar, 500); Inicia após um pequeno atraso
+    }
+
+    digitar();
+});
